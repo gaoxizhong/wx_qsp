547d0403a2247f33fb841a7fb64e9bbdf66e2b9c
gaoxizhong <296203386@qq.com>
2023-09-14 18:17:05 +0800

    优化

--- pages/login_mark/index.js
+++ pages/login_mark/index.js
@@ -205,4 +205,20 @@ Page({
   cancelLogin() {
     this.gotoback();
   },
+
+  //  ==================== 隐私授权 ===================/
+  agree(e){
+    console.log("用户同意隐私授权, 接下来可以调用隐私协议中声明的隐私接口")
+    wx.getClipboardData({
+      success(res) {
+        console.log("getClipboardData success", res)
+      },
+      fail(res) {
+        console.log("getClipboardData fail", res)
+      },
+    })
+  },
+  disagree(e){
+    console.log("用户拒绝隐私授权, 未同意过的隐私协议中的接口将不能调用")
+  },
 })
